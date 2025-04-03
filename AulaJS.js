5ffd27feffc1b2c8cf5464cf3bebde1d1ebd0fad
YagohBR <yagohbergamin1@gmail.com>
2025-04-03 11:29:54 -0400

    AulaJS

---
+++ AulaJS.js
@@ -0,0 +1,32 @@
+
+let abacaxi = 10
+let limao = 5
+let resultado
+
+resultado = abacaxi + limao
+
+console.log(resultado, "Soma")
+
+let maça = 10
+let banana = 9
+let resultadoM
+
+resultadoM = maça * banana
+
+console.log(resultadoM, "Multiplicação")
+
+let pera = 6
+let kiwi = 2
+let resultadoD
+
+resultadoD = pera / kiwi
+
+console.log(resultadoD, "Divisão")
+
+let jaca = 8
+let tomate = 4
+let resultadoSUB
+
+resultadoSUB = jaca - tomate
+
+console.log(resultadoSUB, "Subtração")
