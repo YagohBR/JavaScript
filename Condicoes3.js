a9e0ac875e20df26794dfe55c1e459cb45cb9a73
YagohBR <yagohbergamin1@gmail.com>
2025-04-17 11:11:29 -0400

    a

--- Condicoes3.js
+++
@@ -1,9 +0,0 @@
-var idade = 15
-
-   if (idade >= 18) {
-    console.log ("Você é maior de idade.");
- }
-
-   else {
-    console.log ("Você é menor de idade.");
- }
--- Condicoes5.js
+++
@@ -1,4 +0,0 @@
-var base = 10
-var altura = 5
-let resultado = base * altura
- console.log ("A base do retângulo é", resultado)
